34f7df4ab436b8f423100c80b24e4c46aff22418
erlm <erlm@kea.dk>
2024-10-28 15:22:04 +0100

    pagination

--- src/main/java/org/example/jpamanytoonee23a/exception/ResourceNotFoundException.java
+++ src/main/java/org/example/jpamanytoonee23a/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package org.example.jpamanytoonee23a.exception;
 public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
+    //hej er en konstruk
     public ResourceNotFoundException(String message) {
         super(message);
     }
